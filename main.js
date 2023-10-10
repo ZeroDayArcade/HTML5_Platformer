f06e0bfe3d477af23f7e0e4b885935e17a7ba928
zerodayarcade.com <141867962+ZeroDayArcade@users.noreply.github.com>
2023-10-10 04:07:55 -0700

    Menu screens

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var pressedKeys = {};
 window.onkeyup   = function(e) { pressedKeys[e.keyCode] = false; }
 window.onkeydown = function(e) { pressedKeys[e.keyCode] = true;  }
 
+const titleScreen = document.getElementById("title-screen");
+
 const layers = [
     document.getElementById("parallax-mountain-bg"),
     document.getElementById("parallax-mountain-montain-far"),
@@ -47,29 +49,38 @@ let heroDirection = 1;
 let heroJumping = false;
 let heroPeakJumping = false;
 let jumpPressedLastFrame = false;
+let hoverjump = false;
 
 var scrollX = 0;
 
 var resetAnimationTime = Date.now();
 var previousHeroAnimationIndex = 1;
+let currentLevel = [];
+
+let points = 0;
+
+let gameStarted = false;
+let gameOver = false;
+
+let demoComplete = false;
 
 var levels = [
     [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,12,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,8,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,4,1,0,0,0,0,0,1,6,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 8,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 9,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 9,3,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,6,1,0,0,0,1,4,1,0,0,0,0,0,0,0,0,8,3,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+        [ 1,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,9,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,8,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [ 1,3,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,9,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [ 1,1,1,1,1,1,1,1,4,5,6,1,1,1,1,1,1,0,0,0,0,3,2,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1,1,1,1,1,1,3,0,0,0,8,0,0,0,0,8,0,0,0,0,1,1,1,1,2,3,4,5,4,3,2,1,1,1,1,0,0,0,0,5,0,0,0,1,6,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ],
 ];
 
@@ -77,37 +88,48 @@ const SCREEN_WIDTH = 816;
 const SCREEN_HEIGHT = 480;
 const TILE_SIZE = 32;
 let distanceFromFloor = 0;
-let nearestFloorHeight = TILE_SIZE;
+let nearestFloorHeight = -59;
 let newJumpPress = false;
 let attackCompleted = false;
 
 let hero = {
     spriteWidth: 100,
     spriteHeight: 59,
-    levelX: 100,
+    levelX: 140,
     levelY: 480-32-59,
     renderX: 100,
     renderY: 480-32-59,
     velocityY: 0,
 }
 
-/* 64x10 tiles, each 32x32 pixels */
+/* Draw Single Level Tile, 32x32 pixels */
 var drawTile = function(x, y, tileIndex) {
     var sx = (tileIndex*32) - (64*Math.floor(tileIndex/64)*32);
     var sy = Math.floor(tileIndex/64)*32;
     context.drawImage(tiles, sx, sy, 32, 32, x, y, 32, 32);
 }
 
+/* Render Level Tiles */
 var drawLevelTiles = function(level, hTiles, vTiles, scrollX) {
     for (var i=0; i<hTiles; i++) {
         for (var j=0; j<vTiles; j++) {
-            if (level[j][i] != 0) {
-                drawTile(32*i-scrollX, j*32, 0x1BF);
+            if (level[j][i] == 1) {
+                drawTile(32*i-scrollX, j*32, 18+64*3);
+            }
+            else if (level[j][i] == 10) {
+                drawTile(32*i-scrollX, j*32, 49+64*9);
+            }
+            else if (level[j][i] == 12) {
+                drawTile(32*i-scrollX, j*32, 23+64*1);
+            }
+            else if (level[j][i] != 0) {
+                drawTile(32*i-scrollX, j*32, (41+level[j][i])+64*6);
             }
         }
     }
 }
 
+/* Check Single Tile for Collision with Player or other Sprite */
 var checkSpriteTileCollision = function(sprite, tileX, tileY, tileIndex, collisions) {
     if ((sprite.levelX + sprite.spriteWidth >= tileX &&
         sprite.levelX <= tileX + TILE_SIZE) && 
@@ -137,6 +159,7 @@ var checkSpriteTileCollision = function(sprite, tileX, tileY, tileIndex, collisi
         }
 }
 
+/* Check for Collision Between Hero and Level Tiles */
 var checkSpriteTileCollisions = function(sprite, level) {
     let levelWidth = level[0].length;
     let levelHeight = level.length;
@@ -158,17 +181,123 @@ var checkSpriteTileCollisions = function(sprite, level) {
     return collisions;
 }
 
+/* Reset Game and Level */
+var resetGame = function() {
+    hero = {
+        spriteWidth: 100,
+        spriteHeight: 59,
+        levelX: 140,
+        levelY: 480-32-59,
+        renderX: 100,
+        renderY: 480-32-59,
+        velocityY: 0,
+    };
+    currentLevel = JSON.parse(JSON.stringify(levels[0]));
+    scrollX = 0;
+    points = 0;
+    hoverjump = false;
+    heroAnimationIndex = 1;
+    heroDirection = 1;
+    heroJumping = false;
+    heroPeakJumping = false;
+    jumpPressedLastFrame = false;
+    hoverjump = false;
+    scrollX = 0;
+    resetAnimationTime = Date.now();
+    previousHeroAnimationIndex = 1;
+    points = 0;
+    distanceFromFloor = 0;
+    nearestFloorHeight = -59;
+    newJumpPress = false;
+    attackCompleted = false;
+}
+
+/* Title / Menu Screen */
+var titleScreenLoop = function(now, oldTime) {
+    var sx=0;
+    var sy=0;
+    var swidth=384;
+    var sheight=224;
+    context.drawImage(titleScreen, sx, sy, swidth, sheight, 0, 0, 816, 480);
+    context.font = "90px Luminari, fantasy";
+    context.fillStyle = "rgb(242,131,28)";
+    context.fillText("Platform Pillage", 75, 140);
+    context.font = "60px Luminari, fantasy";
+    context.fillStyle = "rgb(255, 247, 227, " + 
+        (0.75+0.15*Math.cos(2*3.1415*(Math.round(now - oldTime)/1000))) + ")";
+    context.fillText("Press Enter", 240, 260);
+    let currentFrame = Math.floor((((Date.now())/100) % 
+        heroAnimations[heroAnimationIndex][1]));
+    context.drawImage(heroAnimations[heroAnimationIndex][0], 
+        currentFrame*hero.spriteWidth, 0, hero.spriteWidth, 
+        hero.spriteHeight, SCREEN_WIDTH/2-100, 280, 
+        200, 59*2);
+    if (pressedKeys["13"]) {
+        gameStarted = true;
+    }
+}
+
+/* Game Over Screen */
+var gameOverScreen = function(now, oldTime) {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 816, 480);
+    context.font = "90px Luminari, fantasy";
+    context.fillStyle = "red";
+    context.fillText("Game Over", 170, 140);
+    context.font = "60px Luminari, fantasy";
+    context.fillStyle = "rgb(255, 247, 227, " + 
+        (0.50+0.10*Math.cos(2*3.1415*(Math.round(now - oldTime)/1000))) + ")";
+    context.fillText("Press ESC to Exit", 170, 260);
+    let currentFrame = Math.floor((((Date.now())/100) % 
+        heroAnimations[11][1]));
+    context.drawImage(heroAnimations[11][0], 
+        currentFrame*hero.spriteWidth, 0, hero.spriteWidth, 
+        hero.spriteHeight, SCREEN_WIDTH/2-100, 280, 
+        200, 59*2);
+    if (pressedKeys["27"]) {
+        resetGame();
+        gameOver = false;
+    }
+}
+
+/* Level / Demo Complete Screen */
+var demoCompleteScreen = function(now, oldTime) {
+    context.fillStyle = "rgb(67, 67, 67)";
+    context.fillRect(0, 0, 816, 480);
+    context.font = "90px Luminari, fantasy";
+    context.fillStyle = "#4BB543";
+    context.fillText("Level Complete", 90, 140);
+    context.font = "60px Luminari, fantasy";
+    context.fillStyle = "white";
+    const zeroPad = (num, places) => String(num).padStart(places, '0');
+    context.fillText("Score: " + zeroPad(points, 5) + " Pts", 180, 220);
+    context.font = "50px Luminari, fantasy";
+    context.fillStyle = "rgb(255, 247, 227, " + 
+        (0.50+0.10*Math.cos(2*3.1415*(Math.round(now - oldTime)/1000))) + ")";
+    context.fillText("Press ESC to Exit", 200, 290);
+    let currentFrame = Math.floor((((Date.now())/100) % 
+        heroAnimations[3][1]));
+    context.drawImage(heroAnimations[3][0], 
+        currentFrame*hero.spriteWidth, 0, hero.spriteWidth, 
+        hero.spriteHeight, SCREEN_WIDTH/2-100, 280, 
+        200, 59*2);
+    if (pressedKeys["27"]) {
+        resetGame();
+        demoComplete = false;
+    }
+}
+
+/* Main Game Loop */
 var gameLoop = function(interval) {
-    
 
     /* Clear Screen */
 	context.clearRect(0, 0, canvas.width, canvas.height);
 
     /* Update scroll position */
     if (hero.levelX + hero.spriteWidth/2 >= SCREEN_WIDTH/2 && 
-        scrollX+SCREEN_WIDTH < levels[0][0].length*TILE_SIZE ||
+        scrollX+SCREEN_WIDTH < currentLevel[0].length*TILE_SIZE ||
         (hero.levelX + SCREEN_WIDTH/2 + hero.spriteWidth/2 <= 
-        levels[0][0].length*TILE_SIZE && scrollX > 0)) {
+        currentLevel[0].length*TILE_SIZE && scrollX > 0)) {
         scrollX = hero.levelX - SCREEN_WIDTH/2 + hero.spriteWidth/2;
     }
     hero.renderX = hero.levelX - scrollX;
@@ -190,7 +319,7 @@ var gameLoop = function(interval) {
         }
     }
 
-    drawLevelTiles(levels[0], 51, 15, scrollX);
+    drawLevelTiles(currentLevel, currentLevel[0].length, currentLevel.length, scrollX);
 
     /* Draw Hero */
     let currentFrame = Math.floor((((Date.now()-resetAnimationTime)/100) % 
@@ -216,9 +345,7 @@ var gameLoop = function(interval) {
         levelY: hero.levelY,
         spriteWidth: 50,
         spriteHeight: hero.spriteHeight
-    }, levels[0]);
-
-    let hoverjump = false;
+    }, currentLevel);
 
     /* A */
     if (pressedKeys["65"]) {
@@ -231,8 +358,8 @@ var gameLoop = function(interval) {
 
     /* D */
     if (pressedKeys["68"]) {
-        if (hero.levelX + hero.spriteWidth - 25 < 
-            levels[0][0].length*TILE_SIZE && !collisions.right) {
+        if (hero.levelX + hero.spriteWidth - 25 - 30 < 
+            currentLevel[0].length*TILE_SIZE && !collisions.right) {
             hero.levelX += 240*interval;
         }
         heroAnimationIndex = 3;
@@ -305,7 +432,7 @@ var gameLoop = function(interval) {
     /* Physics */
     if ((hero.velocityY >= 0 && collisions.top)) {
         nearestFloorHeight = SCREEN_HEIGHT-collisions.topY;
-    } else nearestFloorHeight = TILE_SIZE;
+    } else nearestFloorHeight = -hero.spriteHeight;
 
     distanceFromFloor = SCREEN_HEIGHT - 
         nearestFloorHeight - (hero.levelY + hero.spriteHeight);
@@ -347,8 +474,54 @@ var gameLoop = function(interval) {
     }
     hero.levelY += hero.velocityY;
 
+    /* Wings */
+    let touchedWings = {top: false, topY:null, 
+        bottom:false, left: false, right: false};
+    checkSpriteTileCollision({
+        levelX: hero.levelX+25,
+        levelY: hero.levelY,
+        spriteWidth: 50,
+        spriteHeight: hero.spriteHeight
+    }, 82*TILE_SIZE, 13*TILE_SIZE, 10, touchedWings);
+    if (touchedWings.top || touchedWings.bottom || 
+        touchedWings.left || touchedWings.right) {
+        if (currentLevel[13][82] == 10) {
+            points += 800;
+            currentLevel[13][82] = 0;
+        }
+        hoverjump = true;
+    }
+
+    /* Golden Cross */
+    let touchedCross = {top: false, topY:null, 
+        bottom:false, left: false, right: false};
+    checkSpriteTileCollision({
+        levelX: hero.levelX+25,
+        levelY: hero.levelY,
+        spriteWidth: 50,
+        spriteHeight: hero.spriteHeight
+    }, 102*TILE_SIZE, 2*TILE_SIZE, 10, touchedCross);
+    if (touchedCross.top || touchedCross.bottom || 
+        touchedCross.left || touchedCross.right) {
+        if (currentLevel[2][102] == 12) {
+            points += 10000;
+            currentLevel[2][102] = 0;
+        }
+    }
+
     jumpPressedLastFrame = pressedKeys["87"];
     previousHeroAnimationIndex = heroAnimationIndex;
+
+    if (hero.levelX + hero.spriteWidth/2 >= 
+        TILE_SIZE*currentLevel[0].length) {
+        demoComplete = true;
+        gameStarted = false;
+    }
+
+    if (hero.levelY >= SCREEN_HEIGHT) {
+        gameOver = true;
+        gameStarted = false;
+    }
 }
 
 let FPS = 60;
@@ -356,13 +529,28 @@ let interval = 1 / FPS;
 let frameCounter = 0;
 let oldTime = Date.now();
 let previousFrameTime = oldTime;
+currentLevel = JSON.parse(JSON.stringify(levels[0]));
+
+/* Game States */
 setInterval(function() {
     let now = Date.now();
-    gameLoop(interval);
+    if (!gameStarted) {
+        if (gameOver) {
+            gameOverScreen(now, oldTime);
+        } else if (demoComplete){
+            demoCompleteScreen(now, oldTime);
+        } else {
+            titleScreenLoop(now, oldTime);
+        }
+    } else {
+        gameLoop(interval);
+        context.font = "28px Luminari, fantasy";
+        context.fillStyle = "rgb(255, 247, 227)";
+        context.fillText(points + " Pts", 10, 50);
+    }
     previousFrameTime = now;
     frameCounter++;
     if (now - oldTime > 1000) {
-        document.getElementById('fps').innerHTML = frameCounter;
         frameCounter = 0;
         oldTime = Date.now();
     }
